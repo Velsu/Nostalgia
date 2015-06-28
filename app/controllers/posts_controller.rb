2eb17ce9426559dec5e8b6d21d49a95711c69fe3
Velsu <riftmage50@gmail.com>
2015-06-28 20:02:26 +0200

    Style Home Page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
-	before_action :authenticate_user!
+	before_action :authenticate_user!, except: [:index, :show]
 
 
 	def index
