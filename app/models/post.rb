83dbeafbf266fc0d2954d3255c8e924045543985
Velsu <riftmage50@gmail.com>
2015-07-01 21:33:46 +0200

    Add validations to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,7 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
 end
