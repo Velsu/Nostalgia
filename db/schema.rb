8d99d08009164562c5df49945656e2c74c7afeb3
Velsu <riftmage50@gmail.com>
2015-07-03 23:04:22 +0200

    Add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625184655) do
+ActiveRecord::Schema.define(version: 20150703210359) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -35,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150625184655) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
